5ea1b4d029ad1f11113624a0477f9d4d96f1c67f
Barnaby Fenn <barnaby.fenn@gmail.com>
2015-10-21 00:23:07 +0100

    its alive!

--- controller.js
+++ controller.js
@@ -1,56 +1,79 @@
 angular.module('app', [])
-   .controller('controller', function($scope, service) {
+   .controller('controller', function($timeout, $scope, service) {
 
-        service.loadState().then(function(data){
-            console.log('load state finished');
-            $scope.feeds = data;    
-        }) 
+        // page load
+        $scope.init = function() {
+            $scope.updateFeeds();    
+            readPosts = service.loadReadPosts();
+        }
         
+        $timeout($scope.init);
+
         $scope.addFeed = function() {
             console.log('addFeed', $scope.formurl);
-            if (!$scope.formurl) {
-                return;
+            if ($scope.formurl) {
+                service.getFeed($scope.formurl).then(function(data) {
+                    $scope.feeds.push(data);
+                    $scope.formurl = "";
+                    service.saveFeeds($scope.feeds);
+                });    
             }
-
-            service.getFeed($scope.formurl).then(newFeed);
         };
-    
+
+        $scope.updateFeeds = function() {
+            
+            service.loadFeeds().then(function(data){
+                console.log('updateFeeds finished ', data);
+                $scope.feeds = data; 
+                service.saveFeeds($scope.feeds);
+                readPosts = service.loadReadPosts();
+                console.log('read posts: ', readPosts);
+            });
+        } 
+
         $scope.readFeed = function(feed) {
-            console.log('readFeed', feed);
+            console.log('readFeed', feed.title);
             $scope.currentFeed = feed;
             $scope.currentPost = null;
         }
         
         $scope.readPost = function(feed, post) {
-            console.log('readPost', post);
             $scope.currentFeed = feed;
             $scope.currentPost = post;
-            if (!post.read) {
-                if (isNaN($scope.currentFeed.unread)) {
-                    $scope.currentFeed.unread = $scope.currentFeed.entries.length;
-                }
-                    
-                $scope.currentFeed.unread--;
-            }
-                
-            post.read = true;
-            $scope.currentPost = post;
-            console.log('currentPost', post);
+            readPosts[post.link] = true;
+            service.saveReadPosts(readPosts);
+            //console.log('readPost ', readPosts);
         }
         
         $scope.formatDate = function(date) {
             return new Date(date);
         }
         
-        function newFeed(data) {
-            console.log('newFeed', data);
-            var newfeed = {
-                    url : $scope.formurl,
-                    title : data.title,
-                    entries : data.entries,
-                    unread : data.entries.length
-                };
-            $scope.feeds.push(newfeed);
+        $scope.unreadPostsInFeed = function(feed) {
+            var unread = 0;
+            angular.forEach(feed.entries, function(entry) {   
+                if (!postHasBeenRead(entry)) {
+                    unread++;
+                }
+            });
+            return unread;
+        }
+    
+        
+        $scope.markAllAsRead = function() {
+            
+            angular.forEach($scope.feeds, function(feed) {   
+                angular.forEach(feed.entries, function(post) {   
+                    readPosts[post.link] = true;
+                });
+            });
+            service.saveReadPosts(readPosts);
+                                
+        }
+        $scope.postHasBeenRead = postHasBeenRead;
+        
+        function postHasBeenRead(post) {
+            return readPosts[post.link] === true;
         }
     }
 );
@@ -59,19 +82,25 @@ angular.module('app')
     .service('service', ['$q', '$http', function($q, $http) {
         
         var defaultFeeds = [
-            'http://downloads.bbc.co.uk/podcasts/radio4/fricomedy/rss.xml',
-            'http://www.jesusandmo.net/feed/',
-            'http://wtfevolution.tumblr.com/rss',
+            'http://downloads.bbc.co.uk/podcasts/radio4/fricomedy/rss.xml', 
+            'http://www.jesusandmo.net/feed/', 
+            'http://wtfevolution.tumblr.com/rss', 
             'http://www.thedailymash.co.uk/rss.xml'
         ];
         
         // return saved data in localStorage, or a default set if not there
         //
-        this.loadState = function() {
-            console.log('loadState');
+        this.loadFeeds = function() {
+         
             var promises = [];
-
-            angular.forEach(defaultFeeds, function(url) {
+            var feeds = defaultFeeds;
+            var blob = localStorage.getItem('feeds');
+            if (blob != null) {
+                feeds = JSON.parse(blob);
+            }
+            console.log('feeds', feeds);
+            
+            angular.forEach(feeds, function(url) {
                 console.log(url);
                 var promise = getFeed(url);
                 promises.push(promise);
@@ -91,10 +120,9 @@ angular.module('app')
                 .success(function(data) {
                     try {    
                         if (data.responseStatus == 200) {
-                            console.log('got ', data.responseData.feed);
+                            
                             deferred.resolve(data.responseData.feed);
                         } else {
-                            console.log('unhappy with ', data);
                             deferred.reject(data);
                         }
                     } catch (err) {
@@ -108,6 +136,29 @@ angular.module('app')
                 });
             
             return deferred.promise;
+        }
+        
+        this.saveReadPosts = function(readPosts) {
+            saveState('readPosts', readPosts);
+        }
+        
+        this.loadReadPosts = function() {
+            var blob = localStorage.getItem('readPosts') || '{}';
+            return JSON.parse(blob);
+        }
+        
+        this.saveFeeds = function(feeds) {
+            data = [];
+            angular.forEach(feeds, function(feed){
+                data.push(feed.feedUrl);
+            });
+            
+            saveState('feeds', data);
+        }
+        
+        saveState = function(key, data) {
+            console.log('saveState', key, data);
+            localStorage.setItem(key, JSON.stringify(data));
         }
     }
 ]);
