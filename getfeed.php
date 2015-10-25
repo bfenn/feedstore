69e695f6761986e2f25838cef7bd191d830494c2
Barnaby Fenn <barnaby.fenn@gmail.com>
2015-10-25 16:35:34 +0000

    moving

--- getfeed.php
+++ getfeed.php
@@ -6,6 +6,15 @@ if (!isset($_REQUEST['feed'])) {
 header('Content-type: text/html');
 header('Access-Control-Allow-Origin: *');
 
-echo file_get_contents('https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q='.$_REQUEST['feed'].'&num=15');
+$cachefile = 'cache/' . md5($_REQUEST['feed']);
+
+// cache exists and less than one hour old
+if (file_exists($cachefile) && (time()-filemtime($cachefile)) < 3600) {
+    echo file_get_contents($cachefile);
+    return;
+}
+
+file_put_contents($cachefile, file_get_contents('https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q='.$_REQUEST['feed'].'&num=15'));
+echo file_get_contents($cachefile);
 
 ?>
